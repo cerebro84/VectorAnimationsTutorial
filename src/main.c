261e9d1ca8eb31e49191590de8fd309dd48ad7bb
Peppe <cerebro84@gmail.com>
2015-10-12 08:24:12 +0200

    window load / unload

---
+++ src/main.c
@@ -0,0 +1,32 @@
+#include <pebble.h>
+
+static Window *s_main_window;
+
+static void main_window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+
+}
+
+static void main_window_unload(Window *window) {
+
+}
+
+static void init() {
+  s_main_window = window_create();
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload,
+  });
+  window_stack_push(s_main_window, true);
+}
+
+static void deinit() {
+  window_destroy(s_main_window);
+}
+
+int main() {
+  init();
+  app_event_loop();
+  deinit();
+}
